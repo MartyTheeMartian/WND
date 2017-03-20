f88c821c5c5745742f1f9e1718491b70d9fdbccd
Sasha Berkowitz <sasha.b510@gmail.com>
2017-03-20 16:15:21 -0700

    seeding ex

--- migrations/2_weight.js
+++ migrations/2_weight.js
@@ -4,7 +4,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('weight', (table) => {
     table.increments();
     table.integer('user_id').references('id').inTable('users').onDelete('CASCADE').notNullable();
-    table.numeric('weight').defaultTo('').notNullable();
+    table.integer('weight').defaultTo(0).notNullable();
     table.timestamp("created_at").defaultTo(knex.raw('now()')).notNullable();
     table.timestamp("updated_at").defaultTo(knex.raw('now()')).notNullable();
   });
--- seeds/4_exercises.js
+++ seeds/4_exercises.js
@@ -1,19 +1,49 @@
+require('isomorphic-fetch');
+
+const promises = [];
+const filtered = [];
+
+for (let i = 1; i <= 21; i++) {
+  promises.push(fetch(`https://wger.de/api/v2/exercise/?page=${i}`)
+    .then(res => {
+      return res.json();
+    })
+    .then(val => {
+      return val.results.filter(x => x.equipment.length === 0 || x.equipment.indexOf(4) !== -1 || x.equipment.indexOf(5) !== -1 || x.equipment.indexOf(6) !== -1 || x.equipment.indexOf(7) !== -1);
+    })
+  );
+}
+
+Promise
+.all(promises)
+.then(res => {
+  const exercises = [];
+  for (let i = 0; i < promises.length; i++) {
+    exercises.push(res[i]);
+  }
+  return exercises;
+})
+.then((fin) => {
+  return fin.reduce((a, b) => [...a, ...b]);
+})
+.then((val) => {
+  val.forEach(x => filtered.push({
+    name: x.name,
+    description: x.description,
+    status: x.status,
+    sets: 0,
+    reps: 0,
+    created_at: new Date('2016-06-29 14:26:16 UTC'),
+    updated_at: new Date('2016-06-29 14:26:16 UTC')
+    }));
+})
 
 exports.seed = function(knex, Promise) {
 
   return knex('users').del()
     .then(function () {
       return Promise.all([
-        knex('users').insert({
-          id: 1,
-          name: 'sit-ups',
-          descrption: 'Lay down on ground, have arms behind head, legs bent.',
-          status: 0,
-          sets: 5,
-          reps: 20,
-          created_at: new Date('2016-06-29 14:26:16 UTC'),
-          updated_at: new Date('2016-06-29 14:26:16 UTC')
-        })
+        knex('users').insert(filtered)
       ])
       .then(() => {
         return knex.raw("SELECT setval('users_id_seq', (SELECT MAX(id) FROM users))");
