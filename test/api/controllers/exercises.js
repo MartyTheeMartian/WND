e4f913eb9ba791644eea378f5f1f34e83251c1b7
Sasha Berkowitz <sasha.b510@gmail.com>
2017-03-21 16:38:54 -0700

    starting on tests

---
+++ test/api/controllers/exercises.js
@@ -0,0 +1,26 @@
+process.env.NODE_ENV = 'test';
+
+const request = require('supertest');
+const expect = require('chai').expect;
+const app = require('../../../app');
+const knex = require('../../../knex');
+
+describe('GET /exercises', () => {
+
+});
+
+describe('GET /exercises/:id', () => {
+
+});
+
+describe('POST /exercises', () => {
+
+});
+
+describe('PUT /exercises/:id', () => {
+
+});
+
+describe('DELETE /exercises', () => {
+
+});
