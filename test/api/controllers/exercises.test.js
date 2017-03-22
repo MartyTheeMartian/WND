5a558bd989eda6344ee0069150533c2461abf425
Sasha Berkowitz <sasha.b510@gmail.com>
2017-03-22 11:52:26 -0700

    prepping for noon push;

--- test/api/controllers/exercises.test.js
+++ test/api/controllers/exercises.test.js
@@ -1,31 +1,32 @@
 process.env.NODE_ENV = 'test';
 
+const should = require('should');
 const request = require('supertest');
 const expect = require('chai').expect;
 const app = require('../../../app');
-const knex = require('../../../knex');
 
-describe('RESTful API', () => {
-  describe('GET /', () => {
+describe('controllers', () => {
 
-    it ('should respond with a status code of 200', (done) => {
-      request(app)
-      .get('/')
-      .expect(200, done)
-    })
+  describe('exercises', () => {
 
-    it('should respond with a content type of text/html', (done) => {
-      request(app)
-      .get('/')
-      .expect('Content-Type', /text\/html/, done)
-    })
+    describe('GET /', () => {
 
-    it('should respond with a paragraph containing hello', (done) => {
-      request(app)
-      .get('/')
-      .expect('<p>hello</p>', done)
-    })
-  });
+      it ('should respond with a status code of 200', (done) => {
+        request(app)
+        .get('/')
+        .expect(200, done)
+      })
+
+      it('should respond with a content type of text/html', (done) => {
+        request(app)
+        .get('/')
+        // .set('Accept', 'application/json')
+        .expect('Content-Type', /text\/html/, done)
+      })
+
+    });
+  })
+
 });
 
 
