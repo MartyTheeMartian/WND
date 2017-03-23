395122da3d2e0a9f5de6c4ec072001a387e9ef60
Sasha Berkowitz <sasha.b510@gmail.com>
2017-03-22 19:59:09 -0700

    errors in tests

--- test/api/controllers/exercises.test.js
+++ test/api/controllers/exercises.test.js
@@ -1,47 +1,76 @@
+'use strict';
+
 process.env.NODE_ENV = 'test';
 
-const should = require('should');
+const { suite, test } = require('mocha');
 const request = require('supertest');
+const knex = require('../../../knex');
+const assert = require('chai').assert;
 const expect = require('chai').expect;
 const app = require('../../../app');
 
 describe('controllers', () => {
 
+    beforeEach((done) => {
+      knex.migrate.latest()
+      .then(() => {
+        return knex.seed.run();
+      })
+      .then(() => {
+          done();
+      })
+      .catch((err) => {
+          done(err);
+      });
+    });
+
+    afterEach(function(done) {
+      knex.migrate.rollback()
+      .then(() => {
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+    });
+
   describe('exercises', () => {
 
-    describe('GET /', () => {
+    describe('GET /exercises', () => {
 
       it ('should respond with a status code of 200', (done) => {
         request(app)
-        .get('/')
+        .get('/exercises')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
         .expect(200, done)
       })
 
-      it('should respond with a content type of text/html', (done) => {
+      it('should respond with a content type of json', (done) => {
         request(app)
-        .get('/')
-        // .set('Accept', 'application/json')
-        .expect('Content-Type', /text\/html/, done)
+        .get('/exercises')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', 'application/json', done)
       })
 
     });
   })
 
 });
-
-
-xdescribe('GET /exercises/:id', () => {
-
-});
-
-xdescribe('POST /exercises', () => {
-
-});
-
-xdescribe('PUT /exercises/:id', () => {
-
-});
-
-xdescribe('DELETE /exercises', () => {
-
-});
+//
+//
+// xdescribe('GET /exercises/:id', () => {
+//
+// });
+//
+// xdescribe('POST /exercises', () => {
+//
+// });
+//
+// xdescribe('PUT /exercises/:id', () => {
+//
+// });
+//
+// xdescribe('DELETE /exercises', () => {
+//
+// });
